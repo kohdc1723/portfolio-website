2e094ce216a22b8af81e73da7de090abc92a5e3c
kohdc <kohdc1723@gmail.com>
2023-01-18 22:44:25 -0800

    install sanity as backend

--- react-frontend/src/containers/Projects/Projects.js
+++ react-frontend/src/containers/Projects/Projects.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import HorizontalScroll from 'react-horizontal-scrolling';
 
 const Projects = () => {
 	return (
 		<div className="py-3">
 			<div className="text-3xl text-teal-700 font-medium">Projects</div>
+
+			<HorizontalScroll>
+				{}
+			</HorizontalScroll>
 		</div>
-	)
+	);
 }
 
-export default Projects
+export default Projects;
---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8cr2chlq',
+    dataset: 'production'
+  }
+})
