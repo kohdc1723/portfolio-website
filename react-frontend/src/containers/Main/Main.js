e4cd2c549d9c5e6bb1ab9ee41c79887997713dba
kohdc <kohdc1723@gmail.com>
2023-01-22 15:48:34 -0800

    main section grammar correction

--- react-frontend/src/containers/Main/Main.js
+++ react-frontend/src/containers/Main/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
 			<div className="text-center py-3">
 				<h3 className="text-2xl font-bebasneue text-slate-900 py-1 md:text-3xl">Hello, I'm</h3>
 				<h2 className="text-5xl font-bebasneue text-teal-700 font-medium py-1 md:text-6xl">Dong-chan Koh</h2>
-				<h3 className="text-2xl font-bebasneue text-slate-900 py-1 md:text-3xl">seeking for Software Developer job</h3>
+				<h3 className="text-2xl font-bebasneue text-slate-900 py-1 md:text-3xl">seeking a Software Developer job</h3>
 				<p className="text-sm text-slate-500 py-1 leading-6 text-center md:text-lg mx-auto">BCIT CST student with a variety of technical skills and strong ability in problem-solving.<br />Expected to graduate in May 2023 and eager to start a career in tech field.</p>
 			</div>
 
